027d880433591849f90587d2b13feae881e0a0a2
Martin Bureš <m2bures@gmail.com>
2016-03-06 19:59:07 +0100

    added config files + updated gitignore

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+require('es6-promise').polyfill()
+
+var webpack = require("webpack");
+var BowerWebpackPlugin = require("bower-webpack-plugin");
+
+module.exports = {
+    entry : "./app/app.js",
+    output : {
+	path : __dirname,
+	    filename : "./app/dist/js/bundle.js"
+    },
+    module : {
+	    loaders : [ {
+	        test : /\.css$/,
+	        loader : "style!css"
+	    }, {
+	        test : /\.(woff|svg|ttf|eot)([\?]?.*)$/,
+	        loader : "file-loader?name=[name].[ext]"
+	    }, {
+	        test : /\.js$/,
+	        exclude: /(node_modules|bower_components)/,
+	        loader : "babel-loader"
+	    },{
+	        test: /\.png$/,
+            loader: "url-loader",
+            query: { mimetype: "image/png" }
+	    } ]
+    },
+    plugins : [ 
+        new BowerWebpackPlugin({
+	        excludes : /.*\.less/
+        }), 
+        new webpack.ProvidePlugin({
+	        $ : "jquery",
+	        jQuery : "jquery",
+	        vis : "vis"
+        }) 
+    ]
+};
